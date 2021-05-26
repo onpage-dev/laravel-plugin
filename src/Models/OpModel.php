1ea7df2475664b20449776ac66e075cd97886f38
Giacomo Rizzi <gufoes@gmail.com>
2021-05-26 16:32:43 +0200

    removed has factory

--- src/Models/OpModel.php
+++ src/Models/OpModel.php
@@ -1,12 +1,9 @@
 <?php
 namespace OnPage\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class OpModel extends \Illuminate\Database\Eloquent\Model
 {
-    use HasFactory;
-
     public $guarded = [];
     public $timestamps = false;
 }
