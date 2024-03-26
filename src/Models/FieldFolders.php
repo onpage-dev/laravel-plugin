dbac671c02031aa84611853f17543f666003c1f1
Alessio Giacobbe <alessio.giacobbe@onpage.it>
2024-03-26 15:36:09 +0100

    form fields in field folder

--- src/Models/FieldFolders.php
+++ src/Models/FieldFolders.php
@@ -12,12 +12,28 @@ class FieldFolders extends OpModel
     {
         return json_decode($labels ?? '{}', true);
     }
-    
+
     function setLabelsAttribute($labels)
     {
         if (!is_array($labels) && !is_object($labels)) {
             throw new \Exception("Invalid labels");
         }
         $this->attributes['labels'] = json_encode($labels);
+    }
+
+    public function fields()
+    {
+        return $this->belongsToMany(Field::class, FieldFolderField::class, 'folder_id');
+    }
+
+    public function form_fields()
+    {
+        return $this->fields()
+            ->wherePivot('type', 'form_fields');
+    }
+    public function arrow_fields()
+    {
+        return $this->fields()
+            ->wherePivot('type', 'arrow_fields');
     }
 }
