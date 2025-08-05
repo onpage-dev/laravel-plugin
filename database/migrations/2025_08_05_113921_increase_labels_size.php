c2ba785af7365134ce4c7155caa4abd1ac97c67e
Giacomo Rizzi <gufoes@gmail.com>
2025-08-05 12:53:54 +0200

    #fix adjust field folder label size

---
+++ database/migrations/2025_08_05_113921_increase_labels_size.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('op_field_folders', function (Blueprint $table) {
+            $table->string('label', 1000)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+    }
+};
